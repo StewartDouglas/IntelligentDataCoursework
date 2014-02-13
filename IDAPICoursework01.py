46d3b6ca431ac18b220a22a109577bf73b020544
Stewart Douglas <stewart.douglas13@imperial.ac.uk>
2014-02-13 10:38:38 +0000

    Started working on Coursework 2

--- IDAPICoursework01.py
+++ IDAPICoursework01.py
@@ -9,8 +9,6 @@ from numpy import *
 # Function to compute the prior distribution of the variable root from the data set
 def Prior(theData, root, noStates):
     prior = zeros((noStates[root]), float) 
-    # I assume the theData is a list of lists
-    # Calculate an array of probabilties
     # First caluclate the number of times each data point occurs
     for row in range(theData.shape[0]):
         prior[theData[row][root]] += 1;
@@ -39,6 +37,7 @@ def JPT(theData, varRow, varCol, noStates):
     jPT = zeros((noStates[varRow], noStates[varCol]), float )
     for row in range(theData.shape[0]):
         jPT[theData[row][varRow]][theData[row][varCol]]+=1
+    # Now normalise
     for row in range(jPT.shape[0]):
         for col in range(jPT.shape[1]):
             jPT[row][col]/=theData.shape[0]
@@ -46,11 +45,12 @@ def JPT(theData, varRow, varCol, noStates):
 
 # Function to convert a joint probability table to a conditional probability table
 def JPT2CPT(aJPT):
-  # Calcualte the probably of the column/parent variable for each of its states
+  # Calcualte the probability of the column/parent variable for each of its states
   probVarCol = zeros(aJPT.shape[1])
   for col in range(aJPT.shape[1]):
     for row in range(aJPT.shape[0]):
       probVarCol[col] += aJPT[row][col]
+  # Now normalise    
   for col in range(aJPT.shape[1]):
     for row in range(aJPT.shape[0]):
       aJPT[row][col]/=probVarCol[col]
@@ -62,13 +62,13 @@ def Query(theQuery, naiveBayes):
     rootPdf = zeros((naiveBayes[0].shape[0]), float)
 
     for i in range(rootPdf.shape[0]):
-        # Initially the posterior probability is equal to the prior probability
+        # Initially I set the posterior probability equal to the prior probability
         rootPdf[i] = naiveBayes[0][i]
         # Update the posterior probability using relevant conditionals
         for j in range(len(naiveBayes)-1):
             rootPdf[i] *= naiveBayes[j+1][theQuery[j]][i]
 
-    # Calculate the scale factor (1/denominator)
+    # Calculate the denominator
     denominator = 0
     for i in range(rootPdf.shape[0]):
         temp = 1.0
@@ -89,19 +89,8 @@ def Query(theQuery, naiveBayes):
 # Calculate the mutual information from the joint probability table of two variables
 def MutualInformation(jP):
     mi=0.0
-    # First, calculate the marginals
-    marginalA = zeros(len(jp[0]);
-    marginalB = zeros(len(jp));
-    for col in range(len(jp[0]):
-        for row in range(len(jp)):
-            marginalA[col] += jp[col][row]
-    for row in range(len(jp)):
-        for col in range(len(jp[0]):
-            marginalB[row] += jp[row][col] 
-    # Second, calculate the mutual information
-    for row in range(jp.):
-        for col in range(jp.):
-            mi += jp[row][column]*math.log((jp[row][column]/(marginalA[col]*marginalB[row])),2);    
+# Coursework 2 task 1 should be inserted here
+   
 
 # end of coursework 2 task 1
     return mi
@@ -249,7 +238,7 @@ def PrincipalComponents(theData):
 
 noVariables, noRoots, noStates, noDataPoints, datain = ReadFile("Neurones.txt")
 theData = array(datain)
-AppendString("results.txt","Coursework One Results by sd3112")
+AppendString("results.txt","Coursework One Results by Stewart Douglas (sd3112)")
 AppendString("results.txt","") #blank line
 AppendString("results.txt","The prior probability of node 0:")
 prior = Prior(theData, 0, noStates)
@@ -260,7 +249,7 @@ AppendArray("results.txt", cPT)
 AppendString("results.txt","The joint probability matrix P(2&0):")
 jPT = JPT(theData, 2, 0, noStates)
 AppendArray("results.txt", jPT)
-AppendString("results.txt","The conditional probability matrx P(2|0) calculated from P(2&0):")
+AppendString("results.txt","The conditional probability matrix P(2|0) calculated from P(2&0):")
 newCPT = JPT2CPT(jPT)
 AppendArray("results.txt", newCPT)
 
@@ -270,10 +259,10 @@ naiveBayes.append(prior)
 for i in range(5):
     naiveBayes.append(CPT(theData,i+1,0,noStates))
 
-AppendString("results.txt","The results of passing state [4,0,0,0,5] into the naive network:")
+AppendString("results.txt","The result of passing state [4,0,0,0,5] into the naive network:")
 posteriorProbs1 = Query([4,0,0,0,5],naiveBayes)
 AppendList("results.txt", posteriorProbs1)
 
-AppendString("results.txt","The results of passing state [6,5,2,5,5] into the naive network:")
+AppendString("results.txt","The result of passing state [6,5,2,5,5] into the naive network:")
 posteriorProbs2 = Query([6,5,2,5,5],naiveBayes)
 AppendList("results.txt", posteriorProbs2)
